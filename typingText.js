4af076f45e134575c5d3bcede064fd085794e1b1
Joshua Si <joshuasi777@gmail.com>
2024-02-03 04:54:07 -0800

    Fix rotating image

--- typingText.js
+++ typingText.js
@@ -8,26 +8,27 @@ class TypingText extends HTMLElement {
   }
 
   connectedCallback() {
-    this.greetings = JSON.parse(this.getAttribute("greetings")) || [];
+    this.texts = JSON.parse(this.getAttribute("texts")) || [];
+    this.delays = JSON.parse(this.getAttribute("delays")) || [200, 50, 1000];
     this.typeWriter();
   }
 
   typeWriter() {
-    const phrase = this.greetings[this.current];
+    const phrase = this.texts[this.current];
 
     this.charIndex = this.isDeleting ? this.charIndex - 1 : this.charIndex + 1;
-    this.typingDelay = this.isDeleting ? 50 : 200;
+    this.typingDelay = this.isDeleting ? this.delays[1] : this.delays[0];
 
     this.innerHTML =
       `${phrase.slice(0, this.charIndex)}<span class='cursor'></span>`;
 
     if (this.charIndex === phrase.length) {
       this.isDeleting = true;
-      this.typingDelay = 1000;
+      this.typingDelay = this.delays[2];
     } else if (this.charIndex === 0) {
       this.isDeleting = false;
-      this.current = (this.current + 1) % this.greetings.length;
-      this.typingDelay = 1000;
+      this.current = (this.current + 1) % this.texts.length;
+      this.typingDelay = this.delays[2];
     }
 
     setTimeout(() => this.typeWriter(), this.typingDelay);
