88a88fce8c5b40ea1a61d2471972d34ae1099ecb
Joshua Si <joshuasi777@gmail.com>
2024-05-12 01:17:57 -0700

    Fix quote char

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
       <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            I'm Josh Si, software engineer based in the San Francisco Bay Area.
+            I&apos;m Josh Si, software engineer based in the San Francisco Bay Area.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             I am currently focused on Enterprise AI, improving the development experience with the C3 AI Platform.
@@ -20,7 +20,7 @@ export default async function Home() {
             <Image
               src={headshot}
               priority={true}
-              alt=""
+              alt="Headshot of Josh Si"
               sizes="(min-width: 1024px) 32rem, 20rem"
               className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
             />
